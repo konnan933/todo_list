a2bf2ea07751a8cb6f83b494a0a7f3035084922c
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-06-22 20:42:20 +0200

    pipára kattintva eltűnik a feladat megfelelően

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", init);
+
 function QS(elem) {
   return document.querySelector(elem);
 }
@@ -9,17 +10,30 @@ function ID(elem) {
   return document.getElementById(elem);
 }
 
+var showIndex = 0;
+
 function init() {
   QS(".beadas").addEventListener("click", putIn);
 }
 
 function putIn() {
   let txt = ID("szoveg").value;
-  //kezelni kell ha üres az input
-  QS(".show").innerHTML += `<div class="todo"><p>${txt}</p>
-     <div class='pipa'>
-        <div class="teteje"></div>
-        <div class="alja"></div>
+  if (txt != "") {
+    QS(
+      ".show"
+    ).innerHTML += `<div class="todo" id ="t${showIndex}"><p>${txt}</p>
+    <div class='pipa${showIndex}' id ="${showIndex}">
+       <div class="teteje"></div>
+       <div class="alja"></div>
     </div>
-  </div>`;
+    </div>`;
+    $(".show").forEach((element) => {
+      element.addEventListener("click", putOut);
+    });
+    showIndex++;
+  }
+}
+function putOut() {
+  id = event.target.id;
+  ID("t" + id).remove();
 }
